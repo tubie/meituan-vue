9aea9e839f32ba4f979e3de32d53fab1c38ddb1d
linjianfang <linjianfang@mminc-qa.onaliyun.com>
2020-07-06 17:03:40 +0800

    添加路由

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <div id="app">
 
     <nut-navbar
-        style="background-color:#000; color: #fff; height:60px; line-height: 60px;"
+        style="background-color:#000; color: #fff; height:54px; line-height: 54px;"
       :leftShow="false"
       :rightShow="false"
-    >美团</nut-navbar>
+    >title</nut-navbar>
 
-    <!--<router-view/>-->
+    <router-view/>
 
     <nut-tabbar
       @tab-switch="tabSwitch3"
@@ -34,7 +34,7 @@ export default {
             "http://img13.360buyimg.com/uba/jfs/t1/29316/38/1115/3203/5c0f3d61E35d0c7da/9e557f2cb5c9dab6.jpg",
           activeIcon:
             "http://img20.360buyimg.com/uba/jfs/t1/9996/36/8646/4833/5c0f3d61E7c1b7e0f/c98ad61124172e93.jpg",
-          href: "###"
+          href: "/homepage"
         },
         {
           tabTitle: "发现",
@@ -43,17 +43,16 @@ export default {
             "http://img13.360buyimg.com/uba/jfs/t1/10361/35/4713/4643/5c0f3d62E437a3c94/273fd0fb90798f03.jpg",
           activeIcon:
             "http://img14.360buyimg.com/uba/jfs/t1/26604/35/1073/7896/5c0f3d61Eb9f5f184/5f01c938abe4216d.jpg",
-          href: "###"
+          href: "/findpage"
         },
         {
           tabTitle: "订单",
           curr: false,
-          num: 2,
           icon:
             "http://img11.360buyimg.com/uba/jfs/t1/14848/18/1066/3723/5c0f41bdE9f2a38fe/e6ed6768717297fb.jpg",
           activeIcon:
             "http://img30.360buyimg.com/uba/jfs/t1/17538/16/1070/6214/5c0f41bdE4bc9a1db/74cf978e5015454b.jpg",
-          href: "###"
+          href: "/orderpage"
         },
         {
           tabTitle: "我的",
@@ -62,7 +61,7 @@ export default {
             "http://img20.360buyimg.com/uba/jfs/t1/20004/20/1045/3620/5c0f3d61Eaaec1670/9e59db63983b7b9f.jpg",
           activeIcon:
             "http://img14.360buyimg.com/uba/jfs/t1/23967/14/1072/6714/5c0f3d61E0ad8991e/8f741953f6e38f15.jpg",
-          href: "###"
+          href: "/minepage"
         }
       ]
     };
@@ -76,6 +75,10 @@ export default {
 </script>
 
 <style>
+
+.nut-tabbar{
+
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HelloWorld from "../components/HelloWorld";
+import HomePage from "../views/tabbar/HomePage"
+import FindPage from "../views/tabbar/FindPage"
+import OrderPage from "../views/tabbar/OrderPage"
+import MinePage from "../views/tabbar/MinePage"
+
+
+
 Vue.use(VueRouter)
 
 let routes;
 routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HelloWorld
+    path:'/homepage',
+    component:HomePage
+  },
+  {
+    path:'/findpage',
+    component:FindPage
+  },
+  {
+    path:'/orderpage',
+    component:OrderPage
+  },
+  {
+    path:'/minepage',
+    component:MinePage
   },
 ];
 
